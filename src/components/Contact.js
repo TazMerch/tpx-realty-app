1129fbbe9943327874fd946b76972e8468bace9e
Tazmond Merchant <Tazmerch@gmail.com>
2023-05-27 21:20:28 -0400

    Modified styles for page components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,35 @@ import "../styles/Contact.css";
 function Contact() {
   return (
     <>
-      <div className="contact"></div>
+      <div className="contact">
+        <div className="contact_container">
+          <p>
+            Lorem ipsum dolor sit amet, graeco consulatu eu per, no mel causae
+            mentitum. Qui odio impetus veritus ea. Pro facer deserunt ocurreret
+            ea. Putant albucius et mea, libris commodo ea usu, pri graece
+            vivendum erroribus ut. Nec elitr tractatos ad, quando constituam id
+            sea. Vim no diam conceptam suscipiantur, an facer graecis pri. Ius
+            tale soleat legendos ex, duo an imperdiet splendide voluptatibus,
+            sonet timeam saperet at mei. Mei an erat porro, cu diceret conceptam
+            eam, aeque nihil scribentur eu quo. Nec ad suas meliore gloriatur,
+            sale omittantur est cu, ei mel accusam volutpat abhorreant. Quod
+            suavitate intellegat ut mei, ut eum invidunt honestatis, agam
+            legimus theophrastus nam an. Cum tantas aliquid vivendum no, duo at
+            commodo insolens, eum omnes integre noluisse ne. Graece convenire ad
+            eum. Zril latine at eum. Ex iuvaret vivendo voluptaria pri, eu has
+            lorem suscipiantur. Causae malorum eos id, pri id omnes iudico.
+            Praesent inciderint vim ne, per at quot explicari, et nam esse
+            detraxit intellegebat. Mutat viris dolorum ea cum. Mundi tempor
+            electram vim ex, agam velit inciderint sed ei. Duo an malis nonumes,
+            eu eos iusto graeci adversarium. Qui eu invenire recteque
+            consectetuer, ridens reformidans qui no. Nam ut wisi antiopam
+            sadipscing, mel luptatum salutatus eu, saperet voluptua eu has. An
+            unum saepe omnium vis, at quot probatus democritum sea. Quo id
+            delectus assueverit eloquentiam, te dicam antiopam imperdiet nam.
+            Persecuti efficiendi inciderint ne nam, sit ut sapientem splendide.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
